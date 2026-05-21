496db44ef93e1e5914fbb40c657f06106aeed6e5
陈云翰 <chenyunhank5@gmail.com>
2026-05-21 17:15:46 -0400

    Update settings.py

--- us_platform/settings.py
+++ us_platform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1e2v%0yl*6&#n1-*y$1f=e9xhom^)#z48_2vx!foo)7i7@ddp4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
