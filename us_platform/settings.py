a737e36181f4438103808aba40c1ce1ced61e104
陈云翰 <chenyunhank5@gmail.com>
2026-05-21 16:52:40 -0400

    Update settings.py

--- us_platform/settings.py
+++ us_platform/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-USE_POSTGRES = os.getenv("USE_POSTGRES", "0") == "1"
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+USE_POSTGRES = os.getenv("USE_POSTGRES", "0") == "1"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
