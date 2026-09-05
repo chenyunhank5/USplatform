142bada5614eb7a8b8d6af7c2430fe91b161f7a6
chenyunhank5 <chenyunhank5@gmail.com>
2026-09-06 03:17:47 +0630

    Add staff message notification badge and sound

--- core/context_processors.py
+++ core/context_processors.py
@@ -12,12 +12,15 @@ def site_settings(request):
 
     unread_support_count = 0
     unread_system_count = 0
+    staff_unread_support_count = 0
 
-    if (
-        request.user.is_authenticated
-        and not request.user.is_staff
-        and request.path.startswith("/user/")
-    ):
+    if request.user.is_authenticated and request.user.is_staff:
+        staff_unread_support_count = SupportMessage.objects.filter(
+            sender__is_staff=False,
+            is_read_by_staff=False,
+        ).count()
+
+    elif request.user.is_authenticated and request.path.startswith("/user/"):
         unread_support_count = SupportMessage.objects.filter(
             user=request.user,
             is_read_by_user=False,
@@ -41,4 +44,5 @@ def site_settings(request):
         "unread_message_count": unread_message_count,
         "unread_support_count": unread_support_count,
         "unread_system_count": unread_system_count,
+        "staff_unread_support_count": staff_unread_support_count,
     }
